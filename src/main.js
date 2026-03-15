b9a996ac6c2427c336f0bb1d3bc8731ae6dc614a
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-03-15 18:23:01 +0100

    feat: show welcome message

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const os = require("os");
+
+let currentWorkingDirectory = os.homedir();
+
+console.log("Welcome to Data Processing CLI!");
+console.log(`You are currently in ${currentWorkingDirectory}`);
